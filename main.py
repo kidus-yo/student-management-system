e94be3ac28fa3e0a75211a016f8a1652338ff975
kidus-yo <kidusyonas96@gmail.com>
2026-07-12 23:56:45 +0300

    Updated

---
+++ main.py
@@ -0,0 +1,31 @@
+#Student Management system
+def main_menu():
+   print("Welcome to Student Managment System💢")
+   print("Main Menu: ")
+   print("1. Add Student")
+   print("2. View Student")
+   print("3. Search Student")
+   print("4. Update Student")
+   print("5. Delete Student")
+   print("6. View top Student")
+   print("7. View Lowest Student")
+   print("8. Calculate Class average")
+   print("9. Total Students")
+   print("10. Clear Records")
+   print("11. Exit")
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+  main_menu()
+main()
