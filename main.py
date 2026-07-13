ba45d2310005595befdb82b57f1b76a01e77117d
kidus-yo <kidusyonas96@gmail.com>
2026-07-13 12:49:25 +0300

    Updated

--- main.py
+++ main.py
@@ -2,5 +2,12 @@
 from menu import *
 
 def main():
+ while True:
   choice = main_menu()
+  if choice == 1:
+    add_student()
+  elif choice == 2:
+   search_student()
+  elif choice == 11:
+   break
 main()
--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+from students import *
+index = 0
 def main_menu():
    print("Welcome to Student Managment System💢")
    print("Main Menu: ")
@@ -16,3 +18,29 @@ def main_menu():
    choice = int(input("Please Enter your choice?: "))
    return choice
 
+def add_student():
+   name = input("Enter Name: ")
+   student_ID = int(input("Enter an ID: "))
+   age = int(input("Enter age: "))
+   department = input("Enter department: ")
+   math = input("Enter Math grade: ")
+   physics = int(input("Enter Physics grade: "))
+   chemistry = int(input("Enter Chemistry grade: "))
+   programming = int(input("Enter Programming grade: "))
+
+   students = {"name": name,
+            "ID" : student_ID,
+            "age" : age,
+            "department": department,
+            "math": math,
+            "physics": physics,
+            "chemistry": chemistry,
+            "programming": programming, 
+            }
+
+   studnets_list.append(students)
+            
+def search_student():
+   id = int(input("Enter the ID of the student: "))
+
+   
