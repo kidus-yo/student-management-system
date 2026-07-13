9af7f5bf78b99a781e4b95c1975baeef3516207c
kidus-yo <kidusyonas96@gmail.com>
2026-07-13 11:00:27 +0300

    Updated

---
+++ menu.py
@@ -0,0 +1,18 @@
+def main_menu():
+   print("Welcome to Student Managment System💢")
+   print("Main Menu: ")
+   print("1. Add Student")
+   print("2. View Student")
+   print("3. Search Student")
+   print("4. Update Student")
+   print("5. Delete Student")
+   print("6. View top Student")
+   print("7. View Lowest Student")
+   print("8. Calculate Class average")
+   print("9. Total Students")
+   print("10. Clear Records")
+   print("11. Exit")
+
+   choice = int(input("Please Enter your choice?: "))
+   return choice
+
